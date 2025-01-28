8c7836e7dfdfe8676e703e37a9a18ffc3e458a86
hrm4k3r <hebertdev82@gmail.com>
2025-01-28 16:35:47 -0300

    fix: adjusting nav menu

--- app/includes/header.jsx
+++ app/includes/header.jsx
@@ -13,14 +13,13 @@ export default function Header() {
       <Navbar />
       <header className="bg-white shadow-md">
         <div className="max-w-7xl mx-auto px-4 flex items-center justify-between py-4">
-          {/* Logo */}
+
           <div className="flex items-center">
             <div className="p-2 rounded-l-md">
               <Image src="/logo.png" alt="Cartão Desconto Legal" width={150} height={100} />
             </div>
           </div>
 
-          {/* Ícone do menu mobile */}
           <button
             className="md:hidden text-2xl focus:outline-none"
             onClick={() => setMenuOpen(!menuOpen)}
@@ -28,9 +27,8 @@ export default function Header() {
             {menuOpen ? <FiX /> : <FiMenu />}
           </button>
 
-          {/* Navegação */}
           <nav
-            className={`absolute md:relative top-[80px] left-0 w-full md:w-auto bg-white md:bg-transparent shadow-md md:shadow-none p-5 md:p-0 md:flex space-x-8 text-lg font-medium transition-all duration-300 ${menuOpen ? "block" : "hidden md:flex"
+            className={`absolute md:relative  left-0 w-full md:w-auto bg-white md:bg-transparent shadow-md md:shadow-none p-5 md:p-0 md:flex space-x-8 text-lg font-medium transition-all duration-300 ${menuOpen ? "block" : "hidden md:flex"
               }`}
           >
             <Link href="/" className="block md:inline hover:text-bigOrange">Home</Link>
@@ -40,7 +38,6 @@ export default function Header() {
             <Link href="/pages/contato" className="block md:inline hover:text-bigOrange">Contato</Link>
           </nav>
 
-          {/* Botões de Ação */}
           <div className="hidden md:flex space-x-4">
             <button className="flex items-center px-6 py-4 bg-bigOrange text-blackSite font-bold hover:bg-shineYellow hover:text-[#1a1a1a] transition ease-in-out">
               Login <FiArrowRight className="ml-2" />
